98b24be52c3d184d09c1f8652a19d1e71c18f732
Rajiv Krishnan <rajiv.j@trufflecorp.com>
2026-03-18 19:31:24 +0530

    feat: update applications list

--- app/applications/page.tsx
+++ app/applications/page.tsx
@@ -1,13 +1,36 @@
 import styles from './page.module.css';
 
 const applications = [
+    'Green Hydrogen',
     'PV & Energy Storage Test Solutions',
     'Battery Test Solutions',
     'Electrical Vehicle Test Solutions',
     'Fuel Cell Test Solutions',
     'Electrolyzer Test Solutions',
     'Power Semiconductor Test Solutions',
-    'Smart Grid Testing'
+    'Smart Grid Testing',
+    'INDUSTRY 4.0',
+    'AI Server Power Supply',
+    'BMS/Battery Protection Board',
+    '3C Electronics',
+    'Automotive Electronics',
+    'Clean Energy Power Generation',
+    'Energy Storage',
+    'Semiconductor/IC',
+    'Motor Drives',
+    'More Electric Aircraft (MEA)',
+    'Battery and Powertrain validation',
+    'eVTOL development',
+    'Battery management systems',
+    'Powertrain Systems',
+    'Motor emulation',
+    'Microgrid & renewable energy',
+    'Distribution automation',
+    'Datacenters',
+    'Modular multilevel converters',
+    'MIL STD810 Test',
+    'DO160G Test',
+    'Explosion-proof lighting'
 ];
 
 export default function Applications() {
